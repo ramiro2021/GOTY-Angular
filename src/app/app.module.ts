53cf952e5e23ba23526750f872a74cd7a258e9bf
ramiro2021 <ramiroamurua@hotmail.com>
2020-10-02 16:39:40 -0300

    feat: estructura y dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GotyComponent } from './pages/goty/goty.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+// Modulos
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GotyComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafico-barra-horizontal',
+  templateUrl: './grafico-barra-horizontal.component.html',
+  styleUrls: ['./grafico-barra-horizontal.component.css']
+})
+export class GraficoBarraHorizontalComponent {
+
+
+  results: any[] = [
+    {
+      'name': 'JUEGO 1',
+      "value": 20
+    },
+    {
+      "name": "JUEGO 2",
+      "value": 25
+    },
+    {
+      "name": "JUEGO 3",
+      "value": 15
+    }
+  ];
+
+
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
+
+  constructor() {
+
+  }
+
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+}
